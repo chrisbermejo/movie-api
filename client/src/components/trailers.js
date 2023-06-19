55d2dcc54d1413ae1bfc0ebf6cc830cbc89e7a55
chrisbermejo <chrisbermejo1@gmail.com>
2023-06-19 02:46:20 -0400

    adding the overlay on the trailer div

--- client/src/components/trailers.js
+++ client/src/components/trailers.js
@@ -10,61 +10,90 @@ export default function getTrailers() {
                 <div className='trailer-container'>
                     <div className='slide-img-container'>
                         <div className='slide-img'>
-                            <img alt="&quot;What We Do in the Shadows&quot;" class="ipc-image" loading="eager" src="https://m.media-amazon.com/images/M/MV5BYmZkZDRmMWMtODc2Zi00MmIxLThjYTQtYzZiYWUwMWQ0YmZmXkEyXkFqcGdeQXZ3ZXNsZXk@._V1_QL40_QL75_UX500_CR0,0,500,281_.jpg" srcset="https://m.media-amazon.com/images/M/MV5BYmZkZDRmMWMtODc2Zi00MmIxLThjYTQtYzZiYWUwMWQ0YmZmXkEyXkFqcGdeQXZ3ZXNsZXk@._V1_QL40_QL75_UX500_CR0,0,500,281_.jpg 500w, https://m.media-amazon.com/images/M/MV5BYmZkZDRmMWMtODc2Zi00MmIxLThjYTQtYzZiYWUwMWQ0YmZmXkEyXkFqcGdeQXZ3ZXNsZXk@._V1_QL40_QL75_UX750_CR0,0,750,422_.jpg 750w, https://m.media-amazon.com/images/M/MV5BYmZkZDRmMWMtODc2Zi00MmIxLThjYTQtYzZiYWUwMWQ0YmZmXkEyXkFqcGdeQXZ3ZXNsZXk@._V1_QL40_QL75_UX1000_CR0,0,1000,563_.jpg 1000w" sizes="100vw, (min-width: 480px) 68vw, (min-width: 600px) 52vw, (min-width: 1024px) 32vw, (min-width: 1280px) 32vw" width="500" />
+                            <img alt="One Piece;" src="https://m.media-amazon.com/images/M/MV5BZmQ0OGZkYzktZWZiYy00ZDliLThhNmMtODM0ZjU3ZTIxZmM5XkEyXkFqcGdeQXZ3ZXNsZXk@._CR53,74,1570,883_QL75_UY281_CR0,0,500,281_.jpg" width='165px' />
+                        </div>
+                        <div className='slide-img-overlay'>
+                            <div className='slide-img-poster-ribbon'>
+                                <svg class="slide-img-poster-ribbon-svg" width="24px" height="34px" viewBox="0 0 24 34" xmlns="http://www.w3.org/2000/svg" role="presentation">
+                                    <polygon class="svg-polygon-1" fill="#000000" points="24 0 0 0 0 32 12.2436611 26.2926049 24 31.7728343"></polygon>
+                                    <polygon class="svg-polygon-2" points="24 0 0 0 0 32 12.2436611 26.2926049 24 31.7728343"></polygon>
+                                    <polygon class="svg-polygon-3" points="24 31.7728343 24 33.7728343 12.2436611 28.2926049 0 34 0 32 12.2436611 26.2926049"></polygon>
+                                </svg>
+                                <div className='slide-img-poster-ribbon-overlay' >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="ipc-icon ipc-icon--add ipc-icon--inline" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"></path></svg>
+                                </div>
+                            </div>
+                            <div className='slide-img-poster'>
+                                <img alt="One Piece" class="ipc-image" loading="lazy" src="https://m.media-amazon.com/images/M/MV5BYzczMzllN2UtNDJmOS00MmE5LWE2MWYtNGEwODcwMDc2M2YyXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_QL75_UY207_CR4,0,140,207_.jpg" srcset="https://m.media-amazon.com/images/M/MV5BYzczMzllN2UtNDJmOS00MmE5LWE2MWYtNGEwODcwMDc2M2YyXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_QL75_UY207_CR4,0,140,207_.jpg 140w, https://m.media-amazon.com/images/M/MV5BYzczMzllN2UtNDJmOS00MmE5LWE2MWYtNGEwODcwMDc2M2YyXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_QL75_UY311_CR6,0,210,311_.jpg 210w, https://m.media-amazon.com/images/M/MV5BYzczMzllN2UtNDJmOS00MmE5LWE2MWYtNGEwODcwMDc2M2YyXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_QL75_UY414_CR8,0,280,414_.jpg 280w" sizes="50vw, (min-width: 480px) 34vw, (min-width: 600px) 26vw, (min-width: 1024px) 16vw, (min-width: 1280px) 16vw" width="165px" />
+                            </div>
+                        </div>
+                        <div className='slide-img-overlay'>
+                            <div>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path></svg><span class="sc-84f23e63-9 czlABb">1:29</span>
+                            </div>
+                            <div>
+                                <div>
+                                    <span >"One Piece"</span>
+                                    <span >1:29</span>
+                                </div>
+                                <div>The Manga Adaptation Comes to Netflix</div>
+                            </div>
                         </div>
                     </div>
                     <aside>
-                        <div className='up-next-header'>
-                            <span>Up next</span>
-                        </div>
-                        <div className='up-next-container'>
-                            <div className='up-next-item'>
-                                <div className='up-next-item-img'>
-                                    <img src='https://m.media-amazon.com/images/M/MV5BZTc5MzQ2MTQtMmI5My00NzI1LTg2YzAtZDUxMGI4N2VhZDNiXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_QL75_UY414_CR8,0,280,414_.jpg' alt='poster' />
-                                </div>
-                                <a href='https://www.youtube.com/watch?v=0W8XKZQ5Z3E' target='_blank' rel='noopener noreferrer'>
-                                    <div>
-                                        <div className='play-icon-time' >
-                                            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="ipc-icon ipc-icon--play-circle-outline-large-inline ipc-icon--inline sc-d4cb23a2-14 ejjmJF editorial-play-icon" id="iconContext-play-circle-outline-large-inline" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path>
-                                            </svg>
-                                            <div className='time' >1:38</div>
-                                        </div>
-                                        <div className='play-icon-header'>"3 Body Problem"</div>
-                                        <div className='play-icon-subheader'>Watch the trailer</div>
-                                    </div>
-                                </a>
+                        <div>
+                            <div className='up-next-header'>
+                                <span>Up next</span>
                             </div>
-                            <div className='up-next-item'>
-                                <div className='up-next-item-img'>
-                                    <img src='https://m.media-amazon.com/images/M/MV5BZjYxYWVjMDMtZGRjZS00ZDE4LTk0OWUtMjUyOTI4MmYxNjgwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_QL75_UX280_CR0,0,280,414_.jpg' alt='poster' />
-                                </div>
-                                <a href='https://www.youtube.com/watch?v=0W8XKZQ5Z3E' target='_blank' rel='noopener noreferrer'>
-                                    <div>
-                                        <div className='play-icon-time' >
-                                            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="ipc-icon ipc-icon--play-circle-outline-large-inline ipc-icon--inline sc-d4cb23a2-14 ejjmJF editorial-play-icon" id="iconContext-play-circle-outline-large-inline" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path>
-                                            </svg>
-                                            <div className='time' >1:10</div>
-                                        </div>
-                                        <div className='play-icon-header'>The 'Elemental' Cast Get Vocal</div>
-                                        <div className='play-icon-subheader'>Behind the Scences of Disney's Newest M...</div>
+                            <div className='up-next-container'>
+                                <div className='up-next-item'>
+                                    <div className='up-next-item-img'>
+                                        <img src='https://m.media-amazon.com/images/M/MV5BZjYxYWVjMDMtZGRjZS00ZDE4LTk0OWUtMjUyOTI4MmYxNjgwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_QL75_UX280_CR0,0,280,414_.jpg' alt='poster' />
                                     </div>
-                                </a>
-                            </div>
-                            <div className='up-next-item'>
-                                <div className='up-next-item-img'>
-                                    <img src={itemPosterIMG} alt='poster' />
-                                </div>
-                                <a href='https://www.youtube.com/watch?v=0W8XKZQ5Z3E' target='_blank' rel='noopener noreferrer'>
-                                    <div>
-                                        <div className='play-icon-time' >
-                                            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="ipc-icon ipc-icon--play-circle-outline-large-inline ipc-icon--inline sc-d4cb23a2-14 ejjmJF editorial-play-icon" id="iconContext-play-circle-outline-large-inline" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path>
-                                            </svg>
-                                            <div className='time' >1:10</div>
+                                    <a href='https://www.youtube.com/watch?v=0W8XKZQ5Z3E' target='_blank' rel='noopener noreferrer'>
+                                        <div>
+                                            <div className='play-icon-time' >
+                                                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="ipc-icon ipc-icon--play-circle-outline-large-inline ipc-icon--inline sc-d4cb23a2-14 ejjmJF editorial-play-icon" id="iconContext-play-circle-outline-large-inline" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path>
+                                                </svg>
+                                                <div className='time' >1:10</div>
+                                            </div>
+                                            <div className='play-icon-header'>The 'Elemental' Cast Get Vocal</div>
+                                            <div className='play-icon-subheader'>Behind the Scences of Disney's Newest M...</div>
                                         </div>
-                                        <div className='play-icon-header'>"What We Do in the Shadows"</div>
-                                        <div className='play-icon-subheader'>Watch the Season 5 trailer</div>
+                                    </a>
+                                </div>
+                                <div className='up-next-item'>
+                                    <div className='up-next-item-img'>
+                                        <img src='https://m.media-amazon.com/images/M/MV5BZTc5MzQ2MTQtMmI5My00NzI1LTg2YzAtZDUxMGI4N2VhZDNiXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_QL75_UY414_CR8,0,280,414_.jpg' alt='poster' />
                                     </div>
-                                </a>
+                                    <a href='https://www.youtube.com/watch?v=0W8XKZQ5Z3E' target='_blank' rel='noopener noreferrer'>
+                                        <div>
+                                            <div className='play-icon-time' >
+                                                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="ipc-icon ipc-icon--play-circle-outline-large-inline ipc-icon--inline sc-d4cb23a2-14 ejjmJF editorial-play-icon" id="iconContext-play-circle-outline-large-inline" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path>
+                                                </svg>
+                                                <div className='time' >1:38</div>
+                                            </div>
+                                            <div className='play-icon-header'>"3 Body Problem"</div>
+                                            <div className='play-icon-subheader'>Watch the trailer</div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div className='up-next-item'>
+                                    <div className='up-next-item-img'>
+                                        <img src={itemPosterIMG} alt='poster' />
+                                    </div>
+                                    <a href='https://www.youtube.com/watch?v=0W8XKZQ5Z3E' target='_blank' rel='noopener noreferrer'>
+                                        <div>
+                                            <div className='play-icon-time' >
+                                                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="ipc-icon ipc-icon--play-circle-outline-large-inline ipc-icon--inline sc-d4cb23a2-14 ejjmJF editorial-play-icon" id="iconContext-play-circle-outline-large-inline" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path>
+                                                </svg>
+                                                <div className='time' >1:10</div>
+                                            </div>
+                                            <div className='play-icon-header'>"What We Do in the Shadows"</div>
+                                            <div className='play-icon-subheader'>Watch the Season 5 trailer</div>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                         <a href='https://www.imdb.com/trailers/?ref_=hm_hp_sm' className='up-next-footer'>Browse trailers                 <span>></span></a>
