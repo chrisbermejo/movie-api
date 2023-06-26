1176016ad1a89b9dd300b5f1c2d49cccb16b8be7
chrisbermejo <chrisbermejo1@gmail.com>
2023-06-26 16:27:10 -0400

    adding scroll function to genre component

--- client/src/components/genres.js
+++ client/src/components/genres.js
@@ -10,6 +10,21 @@ export default function Genres(props) {
 
     const [movieData, setmovieData] = useState([]);
 
+    const container = document.querySelector(`div[data-set=${props.genre}]`);
+
+    console.log(window.innerWidth-15)
+
+    let clickAmount = 0;
+
+    function next(){
+        container.scrollLeft += 1258;
+    }
+
+    function prev(){
+        container.scrollLeft -= 1258;
+    }
+        
+
     useEffect(() => {
         getGenres().then(setmovieData);
     }, []);
@@ -24,14 +39,38 @@ export default function Genres(props) {
                         </path>
                     </svg>
                 </div>
-                <div className='genre-wrapper'>
+                <div className='genre-wrapper' data-set={props.genre}>
                     {movieData.map(movie => (
-                        <a key={movie.key} href={movie.link}>
-                            <img src={movie.image} alt={movie.title} />
-                        </a>
+                        <div key={movie.key} className='genre-poster-card'>
+                            <div className='genre-poster-card-image'>
+                                <a href={movie.link}>
+                                    <img className='genre-a-image' src={movie.image} alt={movie.title} />
+                                </a>
+                            </div>
+                            <div className='genre-poster-card-info'>
+                                <div className='genre-poster-card-info-rating'>
+                                    <span className='genre-poster-card-info-rating-star-container'>
+                                        <svg className='genre-poster-card-info-rating-star-svg' width="24" height="24" viewBox="0 0 24 24" fill="white"><path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path></svg>
+                                        {movie.rating}
+                                    </span>
+                                    <button className='genre-poster-card-info-rate-button'>
+                                        <span className='genre-postercard-info-rate-button-svg-container'>
+                                            <svg className='genre-poster-card-info-rate-button-svg' width="24" height="24" viewBox="0 0 24 24" fill="white"><path d="M22.724 8.217l-6.786-.587-2.65-6.22c-.477-1.133-2.103-1.133-2.58 0l-2.65 6.234-6.772.573c-1.234.098-1.739 1.636-.8 2.446l5.146 4.446-1.542 6.598c-.28 1.202 1.023 2.153 2.09 1.51l5.818-3.495 5.819 3.509c1.065.643 2.37-.308 2.089-1.51l-1.542-6.612 5.145-4.446c.94-.81.45-2.348-.785-2.446zm-10.726 8.89l-5.272 3.174 1.402-5.983-4.655-4.026 6.141-.531 2.384-5.634 2.398 5.648 6.14.531-4.654 4.026 1.402 5.983-5.286-3.187z"></path></svg>
+                                        </span>
+                                    </button>
+                                </div>
+                                <div className='genre-poster-card-info-title'>
+                                    <span className='genre-poster-card-info-title-span'>
+                                        {movie.title}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
                     ))
                     }
                 </div>
+                <button onClick={() => prev()} id='prev-button'>PREV</button>
+                <button onClick={() => next()} id='next-button'>NEXT</button>
             </div>
         </>
     );
