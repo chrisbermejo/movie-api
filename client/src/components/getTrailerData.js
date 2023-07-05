a3f17d7b0cd0bfdcef1f9e0c06646eff5c101ad1
chrisbermejo <chrisbermejo1@gmail.com>
2023-07-04 23:55:35 -0400

    only thing left is to add aside scrolling

---
+++ client/src/components/getTrailerData.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef } from 'react';
+import { useQuery } from 'react-query';
+
+const fetchData = async () => {
+    const response = await fetch('http://localhost:4000/api/trailer');
+    const data = await response.json();
+    return data;
+};
+
+
+const YourComponent = ({ onDataFetched }) => {
+    const { isLoading, data } = useQuery('data', fetchData);
+    const isDataFetched = useRef(false);
+
+    useEffect(() => {
+        if (!isLoading && data && !isDataFetched.current) {
+            onDataFetched();
+            isDataFetched.current = true;
+        }
+    }, [isLoading, data, onDataFetched]);
+
+    if (isLoading) {
+        return (
+            <>
+                <div className="loading-animation">
+                    <div className="loader">
+                        <div className="loader-wheel"></div>
+                        <div className="loader-text"></div>
+                    </div>
+                </div>
+            </>
+        )
+    };
+
+    return (
+        <>
+            {data && data.map((trailer, index) => (
+                <div className='slide-trailer-card' key={trailer.key} datatype={trailer.key}>
+                    <div className='slide-img'>
+                        <img className='slide-img-MAIN' alt={trailer.title} srcSet={trailer.imageURL} width='165px' />
+                        <div className='slide-img-overlay-container'>
+                            <div className='slide-img-overlay-text'>
+                                <div className='slide-img-overlay-text-play'>
+                                    <svg width="24" height="24" viewBox="0 0 24 24"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path></svg>
+                                    <span className='slide-img-overlay-text-play-time' >{trailer.time}</span>
+                                </div>
+                                <div className='slide-img-ovelay-text-play-header-con'>
+                                    <div className='slide-img-ovelay-text-play-header'>
+                                        <span>{trailer.title}</span>
+                                        <span >{trailer.time}</span>
+                                    </div>
+                                    <div className='slide-img-ovelay-text-play-subheader'>{trailer.subText}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='slide-img-overlay'>
+                            <div className='slide-img-poster-ribbon'>
+                                <svg className="slide-img-poster-ribbon-svg" width="24px" height="34px" viewBox="0 0 24 34">
+                                    <polygon className="svg-polygon-1" fill="#000000" points="24 0 0 0 0 32 12.2436611 26.2926049 24 31.7728343"></polygon>
+                                    <polygon className="svg-polygon-2" points="24 0 0 0 0 32 12.2436611 26.2926049 24 31.7728343"></polygon>
+                                    <polygon className="svg-polygon-3" points="24 31.7728343 24 33.7728343 12.2436611 28.2926049 0 34 0 32 12.2436611 26.2926049"></polygon>
+                                </svg>
+                                <div className='slide-img-poster-ribbon-overlay' >
+                                    <svg width="24" height="24" viewBox="0 0 24 24" ><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"></path></svg>
+                                </div>
+                            </div>
+                            <div className='slide-img-poster'>
+                                <img alt={trailer.title} srcSet={trailer.posterURL} width="165px" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default YourComponent;
--- client/src/components/trailersv2.js
+++ client/src/components/trailersv2.js
@@ -1,10 +1,12 @@
 import './trailersv2.css';
 import React, { useState, useEffect, useRef } from 'react';
+import YourComponent from '../components/getTrailerData';
 import itemPosterIMG from '../images/item-poster.jpg';
 
 export default function GetTrailers() {
 
     const containerRef = useRef(null);
+
     const [counter, setCounter] = useState(0);
 
     const intervalRef = useRef(null);
@@ -91,36 +93,43 @@ export default function GetTrailers() {
         });
     }
 
+    const handleDataFetched = () => {
+        setCounter(() => {
+            startInterval();
+            scrollToChild(1, 0);
+            return 1;
+        });
+    };
+
     useEffect(() => {
         const handleResize = () => {
             const elements = document.getElementsByClassName('slide-img-MAIN');
+            const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+
             if (window.innerWidth <= 1024) {
-                console.log(elements.length);
                 for (let i = 0; i < elements.length; i++) {
-                    elements[i].style.width = `${window.innerWidth - 31}px`;
+                    elements[i].style.width = `${window.innerWidth - scrollbarWidth}px`;
                 }
             } else {
                 for (let i = 0; i < elements.length; i++) {
-                    elements[i].style.width = ``;
+                    elements[i].style.width = '';
                 }
             }
         };
 
-        const handleButtonClick = (index) => {
+        const handleDataFetched = () => {
             setCounter(() => {
-                const newCount = index;
-                scrollToChild(newCount, 0);
-                return newCount;
+                scrollToChild(1, 0);
+                return 1;
             });
         };
 
-        getTrailersData().then(setTrailerData);
         handleResize();
 
         window.addEventListener('resize', () => {
             stopInterval();
             handleResize();
-            handleButtonClick(0)
+            handleDataFetched();
         });
         return () => {
             window.addEventListener('resize', () => {
@@ -130,14 +139,6 @@ export default function GetTrailers() {
             stopInterval();
         };
     }, []);
-
-    async function getTrailersData() {
-        const response = await fetch('http://localhost:4000/api/trailer/');
-        const data = await response.json();
-        return data;
-    }
-
-    const [trailerData, setTrailerData] = useState([]);
 
     return (
         <>
@@ -156,43 +157,7 @@ export default function GetTrailers() {
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M5.622.631A2.153 2.153 0 0 0 5 2.147c0 .568.224 1.113.622 1.515l8.249 8.34-8.25 8.34a2.16 2.16 0 0 0-.548 2.07c.196.74.768 1.317 1.499 1.515a2.104 2.104 0 0 0 2.048-.555l9.758-9.866a2.153 2.153 0 0 0 0-3.03L8.62.61C7.812-.207 6.45-.207 5.622.63z"></path></svg>
                         </button>
                         <div className='trailer-wrapper' ref={containerRef}>
-                            {trailerData.map(trailer => (
-                                <div className='slide-trailer-card' key={trailer.key} datatype={trailer.key}>
-                                    <div className='slide-img'>
-                                        <img className='slide-img-MAIN' alt={trailer.title} srcSet={trailer.imageURL} width='165px' />
-                                        <div className='slide-img-overlay-container'>
-                                            <div className='slide-img-overlay-text'>
-                                                <div className='slide-img-overlay-text-play'>
-                                                    <svg width="24" height="24" viewBox="0 0 24 24"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path></svg>
-                                                    <span className='slide-img-overlay-text-play-time' >{trailer.time}</span>
-                                                </div>
-                                                <div className='slide-img-ovelay-text-play-header-con'>
-                                                    <div className='slide-img-ovelay-text-play-header'>
-                                                        <span>{trailer.title}</span>
-                                                        <span >{trailer.time}</span>
-                                                    </div>
-                                                    <div className='slide-img-ovelay-text-play-subheader'>{trailer.subText}</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className='slide-img-overlay'>
-                                            <div className='slide-img-poster-ribbon'>
-                                                <svg className="slide-img-poster-ribbon-svg" width="24px" height="34px" viewBox="0 0 24 34">
-                                                    <polygon className="svg-polygon-1" fill="#000000" points="24 0 0 0 0 32 12.2436611 26.2926049 24 31.7728343"></polygon>
-                                                    <polygon className="svg-polygon-2" points="24 0 0 0 0 32 12.2436611 26.2926049 24 31.7728343"></polygon>
-                                                    <polygon className="svg-polygon-3" points="24 31.7728343 24 33.7728343 12.2436611 28.2926049 0 34 0 32 12.2436611 26.2926049"></polygon>
-                                                </svg>
-                                                <div className='slide-img-poster-ribbon-overlay' >
-                                                    <svg width="24" height="24" viewBox="0 0 24 24" ><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"></path></svg>
-                                                </div>
-                                            </div>
-                                            <div className='slide-img-poster'>
-                                                <img alt={trailer.title} srcSet={trailer.posterURL} width="165px" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
+                            <YourComponent onDataFetched={handleDataFetched} />
                         </div>
                     </div>
                     <aside className='trailer-aside'>
